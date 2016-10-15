5b1e4e8543b8683da927cafa651e9f544bad16ea
joranvar <bart.post@gmail.com>
2016-10-15 18:53:46 +0200

    Add kitchen

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,8 +10,7 @@ module Lib
   , floorPlan
   ) where
 
-import Control.Arrow ((***))
-import Diagrams.Prelude (Diagram, rect, (|||))
+import Diagrams.Prelude (Diagram, rect, (|||), alignL, (#), vcat)
 import Diagrams.Backend.Canvas (B)
 
 data Room = Room Int Int
@@ -19,9 +18,14 @@ data Room = Room Int Int
 render :: Room -> Diagram B
 render (Room w h) = rect (fromIntegral w) (fromIntegral h)
 
-bedroom1, bedroom2 :: Room
+bedroom1, bedroom2, kitchen :: Room
 bedroom1 = Room (120 + 100 + 15) (60 + 100 + 125 + 120)
 bedroom2 = Room (55 + 110 + 50) (60 + 100 + 125 + 120)
+kitchen = Room (60 + 60 + 100 + 15 + 55 + 110 + 50) (100 + 20 + 100)
 
 floorPlan :: Diagram B
-floorPlan = uncurry (|||) . (render *** render) $ (bedroom1, bedroom2)
+floorPlan =
+  vcat
+  [ ((render bedroom1) ||| (render bedroom2)) # alignL
+  , (render kitchen) # alignL
+  ]
