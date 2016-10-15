56b68b158f45fd3304b6818dc90f9f06d89b8aa8
joranvar <bart.post@gmail.com>
2016-10-15 20:05:29 +0200

    Add height lines

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,13 +10,13 @@ module Lib
   , floorPlan
   ) where
 
-import Diagrams.Prelude (Diagram, rect, (|||), alignL, (#), vcat, alignT, atop, alignBR)
+import Diagrams.Prelude
 import Diagrams.Backend.Canvas (B)
 
 data Room = Room Int Int
 
-render :: Room -> Diagram B
-render (Room w h) = rect (fromIntegral w) (fromIntegral h)
+renderRoom :: Room -> Diagram B
+renderRoom (Room w h) = rect (fromIntegral w) (fromIntegral h)
 
 bedroom1, bedroom2, kitchen, bathroom, hall, livingroom, toilet :: Room
 bedroom1 = Room (120 + 100 + 15) (60 + 100 + 125 + 120)
@@ -27,15 +27,29 @@ hall = Room (75 + 100 + 75) (100 + 60 + 100)
 livingroom = Room (148 + 122 + 140) (122 + 200 + 125 + 162 + 142)
 toilet = Room 100 100
 
+rooms :: Diagram B
+rooms =
+  vcat [ ((renderRoom bedroom1) ||| (renderRoom bedroom2)) # alignL
+       , (renderRoom kitchen # alignBR `atop` renderRoom toilet # alignBR) # alignL
+       ] # alignT
+  |||
+  vcat [ renderRoom bathroom
+       , renderRoom hall
+       ] # alignT
+  |||
+  vcat [ renderRoom livingroom
+       ] # alignT
+
+heightLines :: Double -> Double -> Diagram B
+heightLines w _ =
+  vcat [ strutY 122
+       , (hrule w # lw ultraThin # dashingN [0.03,0.03] 0) <> baselineText "183"
+       , strutY (200 + 125 + 162)
+       , (hrule w # lw ultraThin # dashingN [0.03,0.03] 0) <> baselineText "183"
+       ]
+
 floorPlan :: Diagram B
 floorPlan =
-  vcat [ ((render bedroom1) ||| (render bedroom2)) # alignL
-       , (render kitchen # alignBR `atop` render toilet # alignBR) # alignL
-       ] # alignT
-  |||
-  vcat [ render bathroom
-       , render hall
-       ] # alignT
-  |||
-  vcat [ render livingroom
-       ] # alignT
+  heightLines (width rooms) (height rooms) # alignTL
+  `atop`
+  rooms # alignTL
