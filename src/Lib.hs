a593122bd3b8e9c2cfca686d465aa59bdc676cc9
joranvar <bart.post@gmail.com>
2016-10-15 19:37:01 +0200

    Add toilet

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,7 +10,7 @@ module Lib
   , floorPlan
   ) where
 
-import Diagrams.Prelude (Diagram, rect, (|||), alignL, (#), vcat, alignT)
+import Diagrams.Prelude (Diagram, rect, (|||), alignL, (#), vcat, alignT, atop, alignBR)
 import Diagrams.Backend.Canvas (B)
 
 data Room = Room Int Int
@@ -18,19 +18,19 @@ data Room = Room Int Int
 render :: Room -> Diagram B
 render (Room w h) = rect (fromIntegral w) (fromIntegral h)
 
-bedroom1, bedroom2, kitchen, bathroom, hall, livingroom :: Room
+bedroom1, bedroom2, kitchen, bathroom, hall, livingroom, toilet :: Room
 bedroom1 = Room (120 + 100 + 15) (60 + 100 + 125 + 120)
 bedroom2 = Room (55 + 110 + 50) (60 + 100 + 125 + 120)
 kitchen = Room (60 + 60 + 100 + 15 + 55 + 110 + 50) (100 + 20 + 100)
 bathroom = Room (75 + 100 + 75) (125 + 120)
 hall = Room (75 + 100 + 75) (100 + 60 + 100)
 livingroom = Room (148 + 122 + 140) (122 + 200 + 125 + 162 + 142)
-
+toilet = Room 100 100
 
 floorPlan :: Diagram B
 floorPlan =
   vcat [ ((render bedroom1) ||| (render bedroom2)) # alignL
-       , (render kitchen) # alignL
+       , (render kitchen # alignBR `atop` render toilet # alignBR) # alignL
        ] # alignT
   |||
   vcat [ render bathroom
